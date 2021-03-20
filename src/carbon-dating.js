0d11820f57a2ac8c7947b10d1f1b0818f84b0590
nikitausaty <nikitausatiy@yandex.ru>
2021-03-20 06:40:29 +0300

    feat: add Dream Team task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,38 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error')
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+// Для определения возраста археологических находок широко применяется **радиоизотопный анализ**. Один из его видов — **радиоуглеродный анализ**.
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+// Примерный возраст образца рассчитывается при помощи измерения **соотношения** между **современной** активностью изотопа С14 и активностью этого же изотопа в **образце**.
+
+// [Почитайте о методе](https://chem.libretexts.org/Bookshelves/Physical_and_Theoretical_Chemistry_Textbook_Maps/Supplemental_Modules_(Physical_and_Theoretical_Chemistry)/Nuclear_Chemistry/Nuclear_Kinetics/Half-Lives_and_Radioactive_Decay_Kinetics#section_2)
+
+// ---
+// <details>
+
+//   <summary>О расчетах</summary>
+//   Вы можете использовать формулу из статьи по ссылке выше. 0.693 — это приблизительное значение натурального логарифма двойки.
+
+// </details>
+
+// ---
+
+// Ваша задача — реализовать функцию `dateSample(sampleActivity)`, которая рассчитывает примерный возраст образца (в годах). Пожалуйста, используйте данные `MODERN_ACTIVITY` и `HALF_LIFE_PERIOD`.
+
+// Параметр функции `sampleActivity` имеет тип `string`. Рассчитаный возраст образца должен иметь тип `number`. Возраст должен быть **целочисленным**. Возраст должен **округлен вверх** (при получении целого числа). В случае неправильного **типа** входного параметра или **несоответствующем** значении активности, или отсутствии аргумента функция должна вернуть `false`.
+
+// Например:
+
+// `dateSample('1') => 22387` (либо `22392`, в зависимости от используемой формулы)
+
+// `dateSample('WOOT!') => false`
+
+// Напишите ваш код в `src/carbon-dating.js`.
+
+const MODERN_ACTIVITY = 15
+const HALF_LIFE_PERIOD = 5730
+
+module.exports = function dateSample(sampleActivity) {
+  throw new CustomError('Not implemented')
+  // sampleActivity = Number(sampleActivity)
+  // console.log(sampleActivity)
+}
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error')
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false
+  let acronym = []
+  for (let m of members) {
+    if (typeof m === 'string') {
+      const arr = m.trim().split('')
+      acronym.push(arr[0])
+    }
+  }
+  let res = acronym.sort().join('').toLocaleUpperCase()
+  let resa = [...res].sort().join('')
+  return resa
+}
