25219f1cc5850f69669049ad5e6dae7423e8c78a
nikitausaty <nikitausatiy@yandex.ru>
2021-03-31 00:01:19 +0300

    feat: add task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,18 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error')
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let digit = 1
+    let max = 1
+
+    arr.forEach((el) => {
+      if (Array.isArray(el)) {
+        let res = digit + this.calculateDepth(el)
+        if (res > max) {
+          max = res
+        }
+      }
+    })
+    return max
   }
-};
+}
--- src/transform-array.js
+++ src/transform-array.js
@@ -14,13 +14,13 @@ module.exports = function transform(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === '--discard-next') {
       i++
-    } else if (arr[i] === '--discard-prev') {
-      if (res.length !== 0 && arr[i - 2] !== '--discard-next') {
-        res.pop()
-      }
     } else if (arr[i] === '--double-next') {
       if (i < arr.length - 1) {
         res.push(arr[i + 1])
+      }
+    } else if (arr[i] === '--discard-prev') {
+      if (res.length !== 0 && arr[i - 2] !== '--discard-next') {
+        res.pop()
       }
     } else if (arr[i] === '--double-prev') {
       if (i > 0 && arr[i - 2] !== '--discard-next') {
@@ -30,6 +30,5 @@ module.exports = function transform(arr) {
       res.push(arr[i])
     }
   }
-
   return res
 }
